1c8fe4beb69af5f492ef4f22bcfc83a908adf2e3
Jake Grogan <jake.kgrogan@gmail.com>
2022-09-01 21:07:54 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import {
     Heading,
     Image,
     Link,
-    Grid,
-    GridItem,
     Icon
 } from '@chakra-ui/react';
 import {
@@ -21,33 +19,32 @@ import { BioSection, BioYear } from '../components/bio';
 const Page = () => {
     return (
         <Layout>
-            <Grid h="100%" gridTemplateColumns="repeat(auto-fit, 500px)">
-                <GridItem>
+            <div className="container">
+                <div className='item'>
                     <Box
                       flexShrink={0}
-                      mt={{ base: 90, md: 90 }}
                       mr={{ md: 10, sm: 10 }}
                       align="center"
+                      className="pic-container"
                     >
                         <Image
                         borderColor="whiteAlpha.800"
                         borderWidth={1}
                         borderStyle="solid"
-                        maxWidth="250px"
                         display="inline-block"
                         borderRadius="full"
                         src="/images/profilepic.jpg"
+                        className='pic'
                         />
                     </Box>
-                </GridItem>
-                <GridItem>
-                    <Box display={{ md: 'flex' }} mt={90}>
-                        <Box flexGrow={1}>
-                            <Heading as="h2" variant="page-title">
+                </div>
+                <div className='item'>
+                    <Box display={{ md: 'flex' }} className="pic-container">
+                        <Box flexGrow={1} >
+                            <Heading as="h2" variant="page-title" className="title">
                             Software Engineer
                             </Heading>
                         </Box>
-                        
                     </Box>
 
                     <Section delay={0.1}>
@@ -57,7 +54,7 @@ const Page = () => {
                         <Paragraph>
                             I&apos;m Jake Grogan, a DCU computer science graduate from Dublin, Ireland. I&apos;m currently working as a software engineer at{' '}<Link href="https://www.evervault.com">Evervault</Link>.
                             I&apos;m working as part of a small, yet growing team, building software that makes it easy for developers to encrypt & process sensitive data without it touching your app in plaintext!
-                            I&apos;m also a certified AWS Solutions Architect! My interests lie in distributed systems, cloud computing, and building great, customer focused solutions! Doing a lot of music in my spare time.
+                            I&apos;m also a certified AWS Solutions Architect! My interests lie in distributed systems, cloud computing, and building great, customer focused solutions! Doing some music in my spare time.
                         </Paragraph>
                     </Section>
                     
@@ -66,33 +63,39 @@ const Page = () => {
                             Bio
                         </Heading>
                         <BioSection>
-                            <BioYear>2017</BioYear>
-                            Interned on a scholarship with Liberty Information Technology as a Software Engineering Intern.
-                        </BioSection>
-                        <BioSection>
-                            <BioYear>2018</BioYear>
-                            Completed my AWS Certified Solutions Architect (Associate) exam to become a certified AWS Solutions Architect.
-                        </BioSection>
-                        <BioSection>
-                            <BioYear>2019</BioYear>
-                            Completed a six month internship at Amazon Web Services as a Software Development Engineer Intern on the DynamoDB team.
-                        </BioSection>
-                        <BioSection>
-                            <BioYear>2020</BioYear>
-                            Published a research paper - &quot;A Multivocal Literature Review of Function-as-a-Service (FaaS) Infrastructures and Implications for Software Developers.&quot; - in Springer for EuroSPI 2020. DOI: <Link href="https://doi.org/10.1007/978-3-030-56441-4_5" target="_blank">10.1007/978-3-030-56441-4_5</Link>
-                        </BioSection>
-                        <BioSection>
-                            <BioYear>2020</BioYear>
-                            Graduated with a Bsc(Hons). Computer Applications from Dublin City University.
+                            <BioYear>2021 - Present</BioYear>
+                            Currently working at Evervault, a startup company in Dublin working on encryption technology.
                         </BioSection>
                         <BioSection>
                             <BioYear>2020 - 2021 (14 months)</BioYear>
                             Worked at Amazon Web Services as a Software Development Engineer working on the DynamoDB team.
                         </BioSection>
                         <BioSection>
-                            <BioYear>2021 - Present</BioYear>
-                            Currently working at Evervault, a startup company in Dublin working on encryption technology.
+                            <BioYear>2020</BioYear>
+                            Graduated with a Bsc(Hons). Computer Applications from Dublin City University.
                         </BioSection>
+                        <BioSection>
+                            <BioYear>2020</BioYear>
+                            Published a research paper - &quot;A Multivocal Literature Review of Function-as-a-Service (FaaS) Infrastructures and Implications for Software Developers.&quot; - in Springer for EuroSPI 2020. DOI: <Link href="https://doi.org/10.1007/978-3-030-56441-4_5" target="_blank">10.1007/978-3-030-56441-4_5</Link>
+                        </BioSection>
+                        <BioSection>
+                            <BioYear>2019</BioYear>
+                            Completed a six month internship at Amazon Web Services as a Software Development Engineer Intern on the DynamoDB team.
+                        </BioSection>
+                        <BioSection>
+                            <BioYear>2018</BioYear>
+                            Completed my AWS Certified Solutions Architect (Associate) exam to become a certified AWS Solutions Architect.
+                        </BioSection>
+                        <BioSection>
+                            <BioYear>2017</BioYear>
+                            Interned on a scholarship with Liberty Information Technology as a Software Engineering Intern.
+                        </BioSection>
+                        
+                        
+                        
+                        
+                        
+                        
                     </Section>
                     <Box>
                             <Section delay={0.3}>
@@ -116,8 +119,8 @@ const Page = () => {
                                 </Link>
                             </Section>
                         </Box>
-                </GridItem>
-            </Grid>
+                </div>
+            </div>
         </Layout>
     )
 }
